4f703274d34369e25bbac00abb5a31532f74eb45
Bilal05476 <ba4762313@gmail.com>
2022-01-22 00:08:07 +0500

    updates

--- kissan-web-app/public/js/script.js
+++ kissan-web-app/public/js/script.js
@@ -7,15 +7,31 @@ labelId.addEventListener("click", () => {
 });
 
 //Weather Fetching
-let cityName = "karachi";
+
+// set variables for weather data
+let city = "karachi";
 let cityGetBtn = document.getElementById("weather-get");
 let weatherField = document.getElementById("weatherField");
-const api_Key = "xxxxxxxxxxxx";
+let weatherDegree = document.querySelector(".weather-degree");
+let cityName = document.querySelector(".city-name");
+let countryName = document.querySelector(".country-name");
+let cityForcast = document.querySelector(".city-forcast");
 
+// api-key for data fetching
+const api_Key = "your-key";
+
+// function call on event listener
 cityGetBtn.addEventListener("click", async () => {
-  cityName = weatherField.value;
-  const data = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api_Key}`
+  city = weatherField.value;
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_Key}&units=metric`
   );
-  console.log(data.body);
+  const data = await response.json();
+
+  const { weather, name, sys, main } = data;
+  const { temp } = main;
+  weatherDegree.innerHTML = temp;
+  cityName.innerHTML = name;
+  countryName.innerHTML = sys.country;
+  weather.map((item) => (cityForcast.innerHTML = item.main));
 });
